14e15cd0ca6a1aa1faf906f015676b73a51619ad
farhana456 <farhanafiha2004@gmail.com>
2025-02-18 15:31:36 +0600

    changess

--- main.py
+++ main.py
@@ -23,6 +23,14 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"],fill="white")
     canvas.itemconfig(card_background,image=card_back_img)
 
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv")
+    next_card()
+
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50,pady=50, bg=BACKGROUND_COLOR )
@@ -43,7 +51,7 @@ unknown_button = Button(image=cross_image,highlightthickness=0, command=next_car
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image,highlightthickness=0, command=next_card)
+known_button = Button(image=check_image,highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
